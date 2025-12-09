9e06226741b8d2c84d7e00f053cccc0a4c836aca
Alexander <sasha.menkeev@mail.ru>
2025-12-09 10:31:35 +0300

    added comments to posts

--- src/entities/post/ui/PostCard.tsx
+++ src/entities/post/ui/PostCard.tsx
@@ -1,16 +1,35 @@
-import type { IPost } from '../../../widgets/PostList/PostList';
+import { useEffect, useState } from 'react';
+import { CommentList } from '../../../widgets/CommentList/ui/CommentList';
+import type { IPost } from '../../../widgets/PostSection/PostSection';
 import styles from './PostCard.module.css';
 
-interface PostCardProps {
-  post: IPost;
-}
+const PostCard = ({ post }: { post: IPost }) => {
+  const [comments, setComments] = useState([]);
 
-const PostCard: React.FC<PostCardProps> = ({ post }) => {
+  useEffect(() => {
+    async function getComments() {
+      try {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`);
+
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+
+        const data = await res.json();
+        setComments(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+    getComments();
+  }, [post.id]);
+
   return (
     <article className={styles.postCard}>
       <div className={styles.content}>
         <h3 className={styles.postTitle}>{post.title}</h3>
         <p className={styles.postBody}>{post.body}</p>
+        <CommentList comments={comments} />
       </div>
     </article>
   );
